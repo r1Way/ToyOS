7b8bdc665902087f6b8b2a5e7d3aad9993012931
r1Way <2142341323@qq.com>
2025-08-11 20:51:43 +0800

    1.opensbi运行完后进入_entry时，就已经是S mode，所以不能使用csrr读取mstatus寄存器，但是sstatus是可以的。 2.修改了kernel.ld中，stack0的位置，改在了start.c中实现。

--- kernel/start.c
+++ kernel/start.c
@@ -1,6 +1,11 @@
 #include "types.h"
 #include "riscv.h"
 
+#define NCPU          8  // maximum number of CPUs
+// entry.S needs one stack per CPU.
+__attribute__ ((aligned (16))) char stack0[4096*16 * NCPU];
+
+
 // 函数声明，定义在main.c中
 void main();
 
