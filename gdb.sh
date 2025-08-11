963881ee50e337bdfeca6b77ac6ae76470b2f637
r1Way <2142341323@qq.com>
2025-08-11 18:21:43 +0800

    相较于上个版本，修改了kernel.ld里stack0指向的位置。功能无异

--- gdb.sh
+++ gdb.sh
@@ -1,7 +1,7 @@
 gdb-multiarch kernel/kernel \
   -ex "set architecture riscv:rv64" \
   -ex "target remote localhost:26000" \
+  -ex "layout split" \
   # -ex "hbreak _entry" \
   # -ex "hbreak spin" \
-  # -ex "layout split" \
   # -ex "continue" 
